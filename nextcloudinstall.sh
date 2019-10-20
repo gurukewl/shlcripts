2881bc5c5bd61fc9224f0b6caab87c23145e6baf
Arijit Bhattacharyya <ariz_b79@yahoo.co.in>
2019-10-20 21:37:10 +0530

    Update nextcloudinstall.sh

--- nextcloudinstall.sh
+++ nextcloudinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 clear
 
 echo " "
@@ -7,8 +7,7 @@ echo "*****************************************************"
 echo "WELCOME TO THE NEXTCLOUD INSTALLATION SCRIPT"
 echo "-----------------------------------------------------"
 echo " "
-echo " This script will set up a ownCloud storage"
-echo "             server on your target server"
+echo " This script will set up a ownCloud storage server on your target server"
 echo " "
 echo "*****************************************************"
 
@@ -29,16 +28,13 @@ apt update
 apt install software-properties-common -y
 add-apt-repository ppa:ondrej/php
 
-
 #Debian-Install
 sudo apt install ca-certificates apt-transport-https -y
 wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
 echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
 
 apt update
-
 clear
-
 sudo apt install apache2 -y
 
 sudo apt install php7.2 php7.2-gd sqlite php7.2-sqlite3 php7.2-curl php7.2-zip php7.2-xml php7.2-mbstring -y
@@ -50,11 +46,8 @@ cd /var/www/html
 curl https://download.nextcloud.com/server/releases/nextcloud-17.0.0.tar.bz2 | sudo tar -jxv
 
 sudo mkdir -p $d/nextcloud/data
-
 sudo chown www-data:www-data /var/www/html/nextcloud/ -R
-
 sudo chown www-data:www-data $d/nextcloud/data -R
-
 #sudo chmod 750 $d/nextcloud/data
 
 clear
