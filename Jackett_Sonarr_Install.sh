2be9d93b8d94a9f5c2c53e926f876d39f02adc5e
Arijit Bhattacharyya <ariz_b79@yahoo.co.in>
2019-10-29 15:03:35 +0530

    Update Jackett_Sonarr_Install.sh

--- Jackett_Sonarr_Install.sh
+++ Jackett_Sonarr_Install.sh
@@ -7,42 +7,27 @@ echo "WELCOME TO THE NEXTCLOUD INSTALLATION SCRIPT"
 echo "-----------------------------------------------------"
 echo " This script will set up a ownCloud storage server on your target server"
 echo "*****************************************************"
-clear
-echo " "
-echo "****************************************************************"
-echo "Please enter the data path you would like to use to access nextCloud"
-echo "****************************************************************"
-read d
-clear
 
-apt update
-#apt upgrade -y
+clear
+cd 
 
 #Ubuntu-Install
-apt install software-properties-common -y
-add-apt-repository ppa:ondrej/php
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+sudo apt update
 
 #Debian-Install
-sudo apt install ca-certificates apt-transport-https -y
-wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+#sudo apt install ca-certificates apt-transport-https -y
+#wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+#echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
 
-apt update
-clear
-sudo apt install apache2 -y
+cd /opt
+wget http://update.sonarr.tv/v2/master/mono/NzbDrone.master.tar.gz
+sudo tar -xf NzbDrone* 
 
-sudo apt install php7.2 php7.2-gd sqlite php7.2-sqlite3 php7.2-curl php7.2-zip php7.2-xml php7.2-mbstring -y
-
-sudo service apache2 restart
-
-cd /var/www/html
-
-curl https://download.nextcloud.com/server/releases/nextcloud-17.0.0.tar.bz2 | sudo tar -jxv
-
-sudo mkdir -p $d/nextcloud/data
-sudo chown www-data:www-data /var/www/html/nextcloud/ -R
-sudo chown www-data:www-data $d/nextcloud/data -R
-#sudo chmod 750 $d/nextcloud/data
+mv sonarr.service /etc/systemd/system/sonarr.service
+sudo systemctl enable /etc/systemd/system/sonarr.service
+sudo service sonarr start
 
 clear
 
@@ -50,5 +35,5 @@ echo " "
 echo "*****************************************************"
 echo "THE INSTALLATION HAS FINSIHED"
 echo "-----------------------------------------------------"
-echo " Please browse to your IP/nextcloud in your browser to access nextCloud"
+echo " Please browse to your IP:8989 in your browser to access Sonarr"
 echo "**************************************************************************************************************"
