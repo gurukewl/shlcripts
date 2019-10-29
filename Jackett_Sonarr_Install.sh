746c0b4065d5ef3087a490d36b314b55ebc252cb
Arijit Bhattacharyya <ariz_b79@yahoo.co.in>
2019-10-29 15:30:17 +0530

    Update Jackett_Sonarr_Install.sh

--- Jackett_Sonarr_Install.sh
+++ Jackett_Sonarr_Install.sh
@@ -3,14 +3,10 @@
 clear
 echo " "
 echo "*****************************************************"
-echo "WELCOME TO THE NEXTCLOUD INSTALLATION SCRIPT"
-echo "-----------------------------------------------------"
-echo " This script will set up a ownCloud storage server on your target server"
+echo " This script will set up Jackett and Sonarr on your target server"
 echo "*****************************************************"
 
-clear
 cd 
-
 #Ubuntu-Install
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
@@ -28,12 +24,17 @@ sudo tar -xf NzbDrone*
 mv sonarr.service /etc/systemd/system/sonarr.service
 sudo systemctl enable /etc/systemd/system/sonarr.service
 sudo service sonarr start
-
 clear
 
+wget --no-check-certificate https://github.com/Jackett/Jackett/releases/download/v0.12.869/Jackett.Binaries.LinuxARM64.tar.gz
+sudo tar -xf Jackett*
+cd Jackett
+sudo ./install_service_systemd.sh
+
 echo " "
-echo "*****************************************************"
+echo "**************************************************************************************************************"
 echo "THE INSTALLATION HAS FINSIHED"
 echo "-----------------------------------------------------"
 echo " Please browse to your IP:8989 in your browser to access Sonarr"
+echo " Please browse to your IP:9117 in your browser to access Jackett"
 echo "**************************************************************************************************************"
